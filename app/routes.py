a8e408436dbdc607109e2e6e44950fe410be44d4
gproia <giulioproia93@gmaile.com>
2019-10-16 10:50:44 +0200

    Insert form login

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 
 from app import app
 from app.models import Song
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -12,3 +13,12 @@ def index():
 def songs():
     songs=Song.query.all()
     return render_template('songs.html', songs=songs)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
